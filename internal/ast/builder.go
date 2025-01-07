1cd71ff6a3bc9a67e4f6e3db782a9fc1a5816508
Attila Olbrich <olbrichattila@gmx.co.uk>
2025-01-07 21:00:01 +0000

    Some renaming, separation

---
+++ internal/ast/builder.go
@@ -0,0 +1,84 @@
+package ast
+
+import (
+	"fmt"
+	"forth/internal/lexer"
+)
+
+// Builder abstracts the concrete implementation of the Builder
+type Builder interface {
+	Build(tokens []lexer.Token) (*Ast, error)
+}
+
+// New creates a new AST builder
+func New() Builder {
+	return &build{}
+}
+
+type build struct {
+	tokens []lexer.Token
+	pos int
+}
+
+// Build convert tokens created with lexer to an AST tree
+func (p *build) Build(tokens []lexer.Token) (*Ast, error) {
+	p.tokens = tokens
+	Ast := &Ast{};
+
+	for {
+		if p.eof() {
+			break;
+		}
+
+		token := p.at()
+		Node, err := p.parse(token);
+		if err != nil {
+			return nil, err
+		}
+
+		if token.GetType() == lexer.TokenTypeFunction {
+			if n, ok := Node.(*NodeFunction); ok {
+				Ast.AddFunction(n.Name, Node)
+			}
+
+			p.pos++;
+			continue
+		}
+		
+		Ast.body = append(Ast.body, Node)
+		p.pos++
+	}
+
+	return Ast, nil
+}
+
+func (p *build) eof() bool {
+	return p.pos >= len(p.tokens) 
+}
+
+func (p *build) next() lexer.Token {
+	p.pos++;
+	return p.tokens[p.pos]
+}
+
+func (p *build) at() lexer.Token {
+	return p.tokens[p.pos]
+}
+
+func (p *build) expect(tt lexer.TokenType) bool {
+	if p.eof() {
+		return false
+	}
+
+	token := p.next()
+	return token.GetType() == tt
+}
+
+func (p *build) parse(token lexer.Token) (Node, error) {
+	parsers := getParsers()
+	if result, ok := parsers[token.GetType()]; ok {
+		return result(p, token)
+	}
+	
+	return nil, fmt.Errorf("token type %d not defined", token.GetType())
+}
--- internal/ast/parsers.go
+++ internal/ast/parsers.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-type parserFunc = func(*parser, lexer.Token) (Node, error)
+type parserFunc = func(*build, lexer.Token) (Node, error)
 
 func getParsers() map[lexer.TokenType]parserFunc {
 	return map[lexer.TokenType]parserFunc{
@@ -23,7 +23,7 @@ func getParsers() map[lexer.TokenType]parserFunc {
 	}
 }
 
-func pushToStack(_ *parser, t lexer.Token) (Node, error) {
+func pushToStack(_ *build, t lexer.Token) (Node, error) {
 	if v, err := strconv.Atoi(t.GetValue()); err == nil {
 		return &NodePush{Value: v}, nil
 	}
@@ -31,35 +31,35 @@ func pushToStack(_ *parser, t lexer.Token) (Node, error) {
 	return nil, fmt.Errorf("incorrect token value %s", t.GetValue())
 }
 
-func parseAdd(_ *parser, _ lexer.Token) (Node, error) {
+func parseAdd(_ *build, _ lexer.Token) (Node, error) {
 	return &NodeAdd{}, nil
 }
 
-func parseSub(_ *parser, _ lexer.Token) (Node, error) {
+func parseSub(_ *build, _ lexer.Token) (Node, error) {
 	return &NodeSub{}, nil
 }
 
-func parseMultiply(_ *parser, _ lexer.Token) (Node, error) {
+func parseMultiply(_ *build, _ lexer.Token) (Node, error) {
 	return &NodeMultiply{}, nil
 }
 
-func parseDup(_ *parser, _ lexer.Token) (Node, error) {
+func parseDup(_ *build, _ lexer.Token) (Node, error) {
 	return &NodeDup{}, nil
 }
 
-func parseDrop(_ *parser, _ lexer.Token) (Node, error) {
+func parseDrop(_ *build, _ lexer.Token) (Node, error) {
 	return &NodeDrop{}, nil
 }
 
-func parseSwap(_ *parser, _ lexer.Token) (Node, error) {
+func parseSwap(_ *build, _ lexer.Token) (Node, error) {
 	return &NodeSwap{}, nil
 }
 
-func printResult(_ *parser, _ lexer.Token) (Node, error) {
+func printResult(_ *build, _ lexer.Token) (Node, error) {
 	return &NodePrintResult{}, nil
 }
 
-func functionToken(p *parser, _ lexer.Token) (Node, error) {
+func functionToken(p *build, _ lexer.Token) (Node, error) {
 	if !p.expect(lexer.TokenTypeName) {
 		return nil, fmt.Errorf("function : should be followed by a function name")
 	}
@@ -83,6 +83,6 @@ func functionToken(p *parser, _ lexer.Token) (Node, error) {
 	return &NodeFunction{Name: nextToken.GetValue(), Body: body}, nil
 }
 
-func name(_ *parser, t lexer.Token) (Node, error) {
+func name(_ *build, t lexer.Token) (Node, error) {
 	return &NodeName{Name: t.GetValue()}, nil
 }

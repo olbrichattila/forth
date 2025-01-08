9cc3cba7cb030508d25347e5140c2264d2d1cf2a
Attila Olbrich <olbrichattila@gmx.co.uk>
2025-01-08 19:44:33 +0000

    Stack error message was incorrectly named, not over but under flow

--- internal/stack/stack.go
+++ internal/stack/stack.go
@@ -1,4 +1,4 @@
-// Package stack implement a simplified Stack manager
+// Package stack implements a simplified Stack manager
 package stack
 
 import (
@@ -18,12 +18,12 @@ type Stacker interface {
 	Drop() error
 }
 
-type stack struct{
+type stack struct {
 	values []int
 }
 
 var (
-	errStackOverflow = errors.New("stack overflow")
+	errStackUnderflow = errors.New("stack underflow")
 )
 
 // Push adds an element to the top of the stack
@@ -33,33 +33,33 @@ func (s *stack) Push(v int) {
 
 // Pop pulls an element from the top of the stack and removes it
 func (s *stack) Pop() (int, error) {
-	valueCount := len(s.values);
+	valueCount := len(s.values)
 	if valueCount == 0 {
-		return 0, errStackOverflow
+		return 0, errStackUnderflow
 	}
 
 	last := s.values[valueCount-1]
-    s.values = s.values[:valueCount-1]
+	s.values = s.values[:valueCount-1]
 
 	return last, nil
 }
 
-// Last returns with the top of the stack, but not removed
+// Last returns the top element of the stack, but does not remove it
 func (s *stack) Last() (int, error) {
-	valueCount := len(s.values);
+	valueCount := len(s.values)
 	if valueCount == 0 {
-		return 0, errStackOverflow
+		return 0, errStackUnderflow
 	}
 	return s.values[valueCount-1], nil
 }
 
-// Drop removes the element from the top of the stack without returning
+// Drop removes the top element of the stack without returning it
 func (s *stack) Drop() error {
-	valueCount := len(s.values);
+	valueCount := len(s.values)
 	if valueCount == 0 {
-		return errStackOverflow
+		return errStackUnderflow
 	}
-	 s.values = s.values[:valueCount-1]
+	s.values = s.values[:valueCount-1]
 
-	 return nil
+	return nil
 }

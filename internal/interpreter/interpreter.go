eea953ad59ee2b870a6065d0275bfd4d01d2fb98
Attila Olbrich <olbrichattila@gmx.co.uk>
2025-01-08 19:41:49 +0000

    Remove some duplicated logic

--- internal/interpreter/interpreter.go
+++ internal/interpreter/interpreter.go
@@ -9,36 +9,28 @@ import (
 
 // New creates a new interpreter "instance"
 func New() Interpreter {
-	return &interpret{
-		stack.New(),
-	}
+	interpreter := &interpret{
+        stack: stack.New(),
+    }
+    interpreter.initFnMap()
+    return interpreter
 }
 
 // Interpreter abstracts out the functionality of code execution
 type Interpreter interface {
 	Execute(executeAST *ast.Ast) error
+
 }
 
 type interpret struct {
 	stack stack.Stacker
+	fnMap map[ast.NodeType]func(ast.Node, *ast.Ast) error
 }
 
 // Execute runs the AST tree
 func (e *interpret) Execute(executeAST *ast.Ast) error {
-	fnMap := map[ast.NodeType]func(ast.Node, *ast.Ast) error{
-		ast.NodeTypePush: e.push,
-		ast.NodeTypeAdd: e.add,
-		ast.NodeTypeSub: e.sub,
-		ast.NodeTypeMultiply: e.multiply,
-		ast.NodeTypePrintResult: e.printResult,
-		ast.NodeTypeName: e.functionCall,
-		ast.NodeTypeDup: e.dup,
-		ast.NodeTypeDrop: e.drop,
-		ast.NodeTypeSwap: e.swap,
-	}
-
 	for _, n := range executeAST.GetBody() {
-		if fn, ok := fnMap[n.GetType()]; ok {
+		if fn, ok := e.fnMap[n.GetType()]; ok {
 			err := fn(n, executeAST)
 			if err != nil {
 				return err
@@ -50,6 +42,20 @@ func (e *interpret) Execute(executeAST *ast.Ast) error {
 	return nil
 }
 
+func (e *interpret) initFnMap() {
+	e.fnMap = map[ast.NodeType]func(ast.Node, *ast.Ast) error{
+		ast.NodeTypePush: e.push,
+		ast.NodeTypeAdd: e.add,
+		ast.NodeTypeSub: e.sub,
+		ast.NodeTypeMultiply: e.multiply,
+		ast.NodeTypePrintResult: e.printResult,
+		ast.NodeTypeName: e.functionCall,
+		ast.NodeTypeDup: e.dup,
+		ast.NodeTypeDrop: e.drop,
+		ast.NodeTypeSwap: e.swap,
+	}
+}
+
 func (e *interpret) push(n ast.Node, _ *ast.Ast) error {
 	if p, ok := n.(*ast.NodePush); ok {
 		e.stack.Push(p.Value)
@@ -59,29 +65,20 @@ func (e *interpret) push(n ast.Node, _ *ast.Ast) error {
 }
 
 func (e *interpret) add(_ ast.Node, _ *ast.Ast) error {
-	val1, err := e.stack.Pop()
+	val1, val2, err := e.pop2()
 	if err != nil {
-		return err
+		return fmt.Errorf("add operation failed: %w", err)
 	}
 
-	val2, err := e.stack.Pop()
-	if err != nil {
-		return err
-	}
 	e.stack.Push(val1 + val2)
 
 	return nil
 }
 
 func (e *interpret) sub(_ ast.Node, _ *ast.Ast) error {
-	val1, err := e.stack.Pop()
+	val1, val2, err := e.pop2()
 	if err != nil {
-		return err
-	}
-
-	val2, err := e.stack.Pop()
-	if err != nil {
-		return err
+		return fmt.Errorf("sub operation failed: %w", err)
 	}
 
 	e.stack.Push(val2 - val1)
@@ -90,14 +87,9 @@ func (e *interpret) sub(_ ast.Node, _ *ast.Ast) error {
 }
 
 func (e *interpret) multiply(_ ast.Node, _ *ast.Ast) error {
-	val1, err := e.stack.Pop()
+	val1, val2, err := e.pop2()
 	if err != nil {
-		return err
-	}
-
-	val2, err := e.stack.Pop()
-	if err != nil {
-		return err
+		return fmt.Errorf("multiply operation failed: %w", err)
 	}
 
 	e.stack.Push(val1 * val2)
@@ -116,12 +108,23 @@ func (e *interpret) printResult(_ ast.Node, _ *ast.Ast) error {
 }
 
 func (e *interpret) functionCall(n ast.Node, callAst *ast.Ast) error {
-	if val, ok := n.(*ast.NodeName); ok {
-		if fn, ok := callAst.GetFunctions()[val.Name]; ok {
-			if callable, ok := fn.(*ast.NodeFunction); ok {
-				e.Execute(callable.Body)
-			}
-		}
+	val, ok := n.(*ast.NodeName)
+	if !ok {
+		return fmt.Errorf("invalid node type for function call: %T", n)
+	}
+
+	fn, ok := callAst.GetFunctions()[val.Name]
+	if !ok {
+		return fmt.Errorf("function %s not found", val.Name)
+	}
+
+	callable, ok := fn.(*ast.NodeFunction)
+	if !ok {
+		return fmt.Errorf("invalid function node type for %s: %T", val.Name, fn)
+	}
+
+	if err := e.Execute(callable.Body); err != nil {
+		return fmt.Errorf("error executing function %s: %w", val.Name, err)
 	}
 
 	return nil
@@ -130,7 +133,7 @@ func (e *interpret) functionCall(n ast.Node, callAst *ast.Ast) error {
 func (e *interpret) dup(_ ast.Node, _ *ast.Ast) error {
 	value, err := e.stack.Last()
 	if err != nil {
-		return err
+		return fmt.Errorf("get last value from stack operation failed: %w", err)
 	}
 
 	e.stack.Push(value)
@@ -139,21 +142,35 @@ func (e *interpret) dup(_ ast.Node, _ *ast.Ast) error {
 }
 
 func (e *interpret) drop(_ ast.Node, _ *ast.Ast) error {
-	return e.stack.Drop()
+	err := e.stack.Drop()
+	if err != nil {
+		return fmt.Errorf("drop operation failed: %w", err)
+	}
+
+	return nil
 }
 
 func (e *interpret) swap(_ ast.Node, _ *ast.Ast) error {
-	val1, err := e.stack.Pop()
+	val1, val2, err := e.pop2()
 	if err != nil {
-		return err
-	}
-
-	val2, err := e.stack.Pop()
-	if err != nil {
-		return err
+		return fmt.Errorf("swap operation failed: %w", err)
 	}
 
 	e.stack.Push(val1)
 	e.stack.Push(val2)
 	return nil
+}
+
+func (e *interpret) pop2() (int, int, error) {
+	val1, err := e.stack.Pop()
+	if err != nil {
+		return 0, 0, fmt.Errorf("value 1 pop operation failed: %w", err)
+	}
+
+	val2, err := e.stack.Pop()
+	if err != nil {
+		return 0, 0, fmt.Errorf("value 2 pop operation failed: %w", err)
+	}
+
+	return val1, val2, nil
 }
